e5f554551f17ec8a0f1ea5bfa66f4eecb05165b1
GustavoLanzasTibirica <138938969+GustavoLanzasTibirica@users.noreply.github.com>
2023-07-15 10:45:18 -0300

    Curso Python
    Curso livre de 1 mês no Senac para aprimoramento e aprendizagem.

---
+++ aula1.py
@@ -0,0 +1,12 @@
+# Criando meu primeiro arquivo python
+
+# print - escreve algo na tela
+print("Hello World!")
+
+# input - Permite a entrada de uma informação
+# input("Digite seu Nome")
+
+# Variáveis - Armazena uma informção na memoria
+nome = input("Digite seu Nome")
+
+print("Seja bem vindo ", nome)
